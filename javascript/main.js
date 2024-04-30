e89d157c2c84840bc9ff05637517c8c3ac16a9f1
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-04-30 16:47:48 +0200

    [UPD] Logic fixed

--- javascript/main.js
+++ javascript/main.js
@@ -4,14 +4,17 @@ const ul = document.getElementById("numbers-list");
 
 for (let index = 1; index <= 100; index++) {
   const li = document.createElement("li");
-  li.append("Ciao");
+  li.append("Elemento");
   ul.append(li);
+  // console.log(index);
 
-  if (li % 3) {
-    console.log("divisibile per 3");
-  } else if (li % 5) {
-    console.log("divisibile per 5");
-  } else if (li % 3 && li % 5) {
+  if (index % 3 == 0 && index % 5 == 0) {
     console.log("divisibile per 3 e per 5");
+  } else if (index % 3 == 0) {
+    console.log("divisibile per 3");
+  } else if (index % 5 == 0) {
+    console.log("divisibile per 5");
+  } else {
+    console.log(index);
   }
 }
